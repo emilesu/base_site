822fbdaf4c46946cb2add5fc9d0a11a55e697abc
emilesu <emilesu@qq.com>
2017-10-25 18:10:04 +0800

    complete project status

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,14 @@
 class Project < ApplicationRecord
 
+  # 数据栏位校验
+  validates :title, presence: true
+  validates :description, presence: true
+
   # 与 user 关系
   belongs_to :user
+
+  # 工程状态
+  STATUS = ["communication", "payment", "development", "text", "complete"]
+  validates_inclusion_of :status, :in => STATUS
 
 end
