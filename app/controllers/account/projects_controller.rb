2096a7df497410bb23bbcf7cfd1ff298b0dd27b9
emilesu <emilesu@qq.com>
2017-10-25 15:14:51 +0800

    complete project index page

--- app/controllers/account/projects_controller.rb
+++ app/controllers/account/projects_controller.rb
@@ -2,8 +2,8 @@ class Account::ProjectsController < ApplicationController
   before_action :authenticate_user!
   layout "account"
 
-  def show
-    @projects = Project.all
+  def index
+    @projects = Project.where(:user_id => current_user.id).order("created_at DESC")
   end
 
   def new
